c0f6bacf8e7cc6597458a82ea1f8cf4070043937
Pablo Arango <pabloearangom@gmail.com>
2023-12-09 18:08:38 -0500

    hw2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
+import React, { useState } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const addContact = (newContact) => {
+    if (contacts.find((contact) => contact.name === newContact.name)) {
+      return false;
+    }
+
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+    return true;
+  };
+
+  const deleteContact = (id) => {
+    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <ContactList contacts={contacts} filter={filter} deleteContact={deleteContact} />
     </div>
   );
 };
+
+export default App;
+
